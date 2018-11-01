4bb2ba17ea2493a7bbf3fb1942235c67fa2e457e
YoungJin Kim <yjkim5426@gmail.com>
2018-11-01 19:41:36 +1300

    call api - done

--- src/App.js
+++ src/App.js
@@ -2,9 +2,37 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state={}
+
+  callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+      .then(response => response.json())
+      .then(json => json.data.movies)
+  }
+
+  getMovies = async () => {
+    const movies = await this.callApi();
+    console.log(movies);
+    this.setState({
+      movies
+    })
+  }
+
+  renderMovies = () => {
+    const movies = this.state.movies.map( movie =>
+      <><h1>{movie.title}</h1><img src={movie.small_cover_image} /></>
+    );
+    return movies
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+  
   render() {
     return (
       <div className="App">
+        {this.state.movies ? this.renderMovies() : 'Loading'}
       </div>
     );
   }
